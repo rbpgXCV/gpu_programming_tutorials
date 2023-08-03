55d4d4b9246d1dcd0ea54daaf8393dfa9e501afd
rbpgXCV <roboticsplayground95@gmail.com>
2023-08-03 10:35:46 -0400

    gitignore and script update

--- download_pytorch_and_build_docker.sh
+++ download_pytorch_and_build_docker.sh
@@ -1,16 +1,19 @@
 xhost local:root
 
-# Download libtorch (C++) with CUDA 11.8 support #
+# Download libtorch (C++) with CUDA 11.8 support 
 wget https://download.pytorch.org/libtorch/cu118/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcu118.zip
 
-# unzip libtorch library #
+# unzip libtorch library 
 unzip libtorch-cxx11*
 
 # remove zip file
 rm -rf libtorch-cxx11*
 
-# copy libtorch to lib folder #
-cp -r libtorch lib/
+# make lib folder
+mkdir -p build
 
+# move libtorch to lib folder 
+mv libtorch lib/
+
+# build docker image
 docker-compose build
-docker-compose up -d 
